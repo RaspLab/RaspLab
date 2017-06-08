eac0deac5ec69fc65a9d53eaf0407bb83dcff450
hellix08 <perantonielia0@gmail.com>
2017-06-08 09:33:40 +0200

    Fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/RaspLab/RaspLab.git
 pip3 install -r RaspLab/requirements.txt
-echo "@python3 $(pwd)/RaspLab/examples-api-use/RaspLab/RaspLab.py">> .config/lxsession/LXDE-pi/autostart
+echo "@python3 $(pwd)/RaspLab/python/RaspLab.py">> .config/lxsession/LXDE-pi/autostart
 reboot
